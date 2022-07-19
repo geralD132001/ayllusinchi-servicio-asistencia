19ea602bf4e1b9110289b6aa12fc849b23cf0d63
GeraldCode <82231832+geralD132001@users.noreply.github.com>
2022-07-19 06:44:46 -0500

    Update AsistenciaController.java

--- src/main/java/com/backend/springboot/app/asistencia/controllers/AsistenciaController.java
+++ src/main/java/com/backend/springboot/app/asistencia/controllers/AsistenciaController.java
@@ -23,7 +23,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "https://ayllusinchi-app.azurewebsites.net" })
 @RestController
 public class AsistenciaController {
 
